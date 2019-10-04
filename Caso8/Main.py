23b7cd0f08b6c6bb711e70f1aeb0656308c98961
Esteban Torres <esteban13torres@gmail.com>
2019-10-03 21:08:11 -0600

    Se añadio para que se divida la imagen en sectores

--- Caso8/Main.py
+++ Caso8/Main.py
@@ -1,12 +1,30 @@
+from Constants import *
 from PIL import Image
 
 
-def openImage(path):
-    newImage = Image.open(path)
+def openImage(pPath):
+    newImage = Image.open(pPath)
     return newImage
 
 
-def main():
-    path = ""
-    image = openImage(path)
-    image.show()
+def createSectors():
+    image = openImage(Images[0])
+    imageWidth, imageHeight = image.size
+    sectorsX = []
+    sectorsY = []
+    numberOfPixelsPerSectorX = imageWidth/numbersOfLines
+    numberOfPixelsPerSectorY = imageHeight/numbersOfLines
+    linePixelPositionX = 0
+    linePixelPositionY = 0
+    for actualNumberOfLines in range(0, numbersOfLines):
+        linePixelPositionX += numberOfPixelsPerSectorX
+        sectorsX += [linePixelPositionX]
+        linePixelPositionY += numberOfPixelsPerSectorY
+        sectorsY += [linePixelPositionY]
+
+    print(sectorsX)
+    print(sectorsY)
+
+
+if __name__ == "__main__" :
+    createSectors()
